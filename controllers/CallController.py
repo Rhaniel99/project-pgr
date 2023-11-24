3c020fd6ec5ee32d5d5d6cecd5934773a3124722
rhaniel99 <rhanielmonteiro.13@gmail.com>
2023-11-24 10:48:23 -0400

    teste

--- controllers/CallController.py
+++ controllers/CallController.py
@@ -1,28 +1,29 @@
 # from typing import List
-import mysql.connector
-from mysql.connector import Error
-from services.database import criar_conexao
+# import mysql.connector
+# from mysql.connector import Error
+# from services.database import criar_conexao
+from data import criar_conexao
 from models.Called import Call
 from datetime import datetime  # Importe o módulo datetime
+import sqlite3
 
-
-def register(Call):
+def register(call):
     try:
         conn = criar_conexao()
         cursor = conn.cursor()
-        
+
         sql = """
         INSERT INTO FormsData(opCalled, repDefect, glpi, locale, status, resp) 
-        VALUES (%s, %s, %s, %s, %s, %s)
+        VALUES (?, ?, ?, ?, ?, ?)
         """
-        value = (Call.opCalled, Call.repDefect, Call.glpi,Call.locale, Call.status, Call.resp)
+        value = (call.opCalled, call.repDefect, call.glpi, call.locale, call.status, call.resp)
 
         cursor.execute(sql, value)
         conn.commit()
 
         return cursor.rowcount
 
-    except mysql.connector.Error as e:
+    except sqlite3.Error as e:
         print('Não foi possível inserir o registro. Erro:', str(e))
     finally:
         if cursor:
@@ -30,35 +31,33 @@ def register(Call):
         if conn:
             conn.close()
 
-def upDate(flag, Call):
+def upDate(flag, call):
     try:
         conn = criar_conexao()
         cursor = conn.cursor()
 
-        # Se o status for "Finalizado", atualize apenas os outros campos
-        if Call.status == "Finalizado":
-              # Se o status não for "Finalizado", atualize todos os campos, incluindo endCalled
-            Call.endCalled = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if call.status == "Finalizado":
+            call.endCalled = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             sql = """
             UPDATE FormsData 
-            SET opCalled=%s, repDefect=%s, glpi=%s, locale=%s, status=%s, resp=%s, endCalled=%s
-            WHERE id=%s
+            SET opCalled=?, repDefect=?, glpi=?, locale=?, status=?, resp=?, endCalled=?
+            WHERE id=?
             """
-            value = (Call.opCalled, Call.repDefect, Call.glpi, Call.locale, Call.status, Call.resp, Call.endCalled, flag)
+            value = (call.opCalled, call.repDefect, call.glpi, call.locale, call.status, call.resp, call.endCalled, flag)
         else:
             sql = """
             UPDATE FormsData 
-            SET opCalled=%s, repDefect=%s, glpi=%s, locale=%s, status=%s, resp=%s
-            WHERE id=%s
+            SET opCalled=?, repDefect=?, glpi=?, locale=?, status=?, resp=?
+            WHERE id=?
             """
-            value = (Call.opCalled, Call.repDefect, Call.glpi, Call.locale, Call.status, Call.resp, flag)
+            value = (call.opCalled, call.repDefect, call.glpi, call.locale, call.status, call.resp, flag)
 
         cursor.execute(sql, value)
         conn.commit()
 
         return cursor.rowcount
 
-    except mysql.connector.Error as e:
+    except sqlite3.Error as e:
         print('Não foi possível atualizar o registro. Erro:', str(e))
     finally:
         if cursor:
@@ -66,16 +65,14 @@ def upDate(flag, Call):
         if conn:
             conn.close()
 
-
-
 def selectOne(id):
     try:
         conn = criar_conexao()
         cursor = conn.cursor()
         value = (id,)
-        
-        cursor.execute("SELECT * FROM FormsData WHERE id = %s", value)
-        
+
+        cursor.execute("SELECT * FROM FormsData WHERE id = ?", value)
+
         rows = cursor.fetchall()
         registros = []
         for row in rows:
@@ -91,9 +88,9 @@ def selectOne(id):
             }
             registros.append(registro)
 
-        return registros[0]
+        return registros[0] if registros else None
 
-    except mysql.connector.Error as e:
+    except sqlite3.Error as e:
         print('Não foi possível executar a consulta. Erro:', str(e))
     finally:
         if cursor:
@@ -123,21 +120,20 @@ def selectAll():
 
         return registros
 
-    except mysql.connector.Error as e:
+    except sqlite3.Error as e:
         print('Não foi possível executar a consulta. Erro:', str(e))
     finally:
         if cursor:
             cursor.close()
         if conn:
             conn.close()
-            
+
 def deleteOne(id):
     conn = criar_conexao()
     cursor = conn.cursor()
-    
-    # Correção aqui, utilizando uma tupla e ajustando a consulta
+
     value = (id,)
-    sql = "DELETE FROM FormsData WHERE id = %s"
-    
+    sql = "DELETE FROM FormsData WHERE id = ?"
+
     cursor.execute(sql, value)
     conn.commit()
